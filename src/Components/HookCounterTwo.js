a58677dd5bada4891fffe8d06927bfb84dedeb1c
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-02-23 12:15:44 +0530

    one feature added to counter

--- src/Components/HookCounterTwo.js
+++ src/Components/HookCounterTwo.js
@@ -6,12 +6,19 @@ import './HookCounterTwo.css'
  const initialCount = 0 
   const[count , setCount] = useState(initialCount)
 
+   const incrementFive = () =>{
+    for(let i =0 ; i<=5 ; i++){
+       setCount(count + i)
+    }
+  }
+
   return (
     <div className='primary'>
     Count : {count}
       <button onClick={() => setCount(initialCount)}> reset </button>
       <button onClick={() => setCount(count + 1)}> increment</button>
       <button onClick={() => setCount(count - 1)}> decrement </button>
+      <button onClick={incrementFive}>increment 5 </button>
     </div>
   )
 }
