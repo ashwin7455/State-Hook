c94a85257036810333684d858ac7ae7a67e95bf9
Ashwin Yadav <104228697+ashwin7455@users.noreply.github.com>
2024-02-23 14:09:25 +0530

    jhf

--- src/Components/HookCounterTwo.js
+++ src/Components/HookCounterTwo.js
@@ -7,17 +7,20 @@ import './HookCounterTwo.css'
   const[count , setCount] = useState(initialCount)
 
    const incrementFive = () =>{
-    for(let i =0 ; i<=5 ; i++){
-       setCount(count + i)
-    }
+    for(let i =0 ; i<5 ; i++){
+        console.log(count)
+       setCount(prevState => prevState +1)
+       console.log(count)
+       
+    }   
   }
 
   return (
     <div className='primary'>
     Count : {count}
       <button onClick={() => setCount(initialCount)}> reset </button>
-      <button onClick={() => setCount(count + 1)}> increment</button>
-      <button onClick={() => setCount(count - 1)}> decrement </button>
+      <button onClick={() => setCount(prevState => prevState + 1)}> increment</button>
+      <button onClick={() => setCount(prevState  => prevState - 1)}> decrement </button>
       <button onClick={incrementFive}>increment 5 </button>
     </div>
   )
